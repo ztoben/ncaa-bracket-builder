4a7fbbd9fb15218f5f0ec98b7a1dc26fe8028818
Zach Toben <zach.toben@gmail.com>
2023-03-17 12:30:48 -0500

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import './globals.scss'
 export const metadata = {
   title: 'NCAA Bracket Builder',
   description: 'Build a bracket for the NCAA tournament',
+  icons: {
+    icon: '/icon.png',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
